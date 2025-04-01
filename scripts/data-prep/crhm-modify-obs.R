9ee3283d7708de257f1d01883253e3ea0b6e50ff
Alex Cebulski <alexcebulski@gmail.com>
2025-03-31 17:41:18 -0700

    updt to use prj files instead of external

--- scripts/data-prep/crhm-modify-obs.R
+++ scripts/data-prep/crhm-modify-obs.R
@@ -56,21 +56,12 @@ ft_obs_irtc_tree <- ft_obs_irtc |>
   mutate(obs_snow_load = ifelse(is.na(obs_snow_load), 9999, obs_snow_load))
 
 CRHMr::writeObsFile(ft_obs_irtc_tree,
-                    '../../analysis/crhm-analysis/obs/ffr_crhm_modelling_obs_w_asm_data.obs',
+                    'crhm/obs/ffr_crhm_modelling_obs_w_asm_data.obs',
                     comment = 'This file consists of air temp, rh, and wind speed from the Waterloo Forest Tower (aka Fortress Forest Ride) see the R proj met-data-processing for qaqc and gap fill procedures. The radiation data is from Fortress Ridge and has been gap filled with Fortress Ridge South.')
 
 obs_event <- ft_obs_irtc_tree |> filter(datetime >= as.POSIXct('2022-03-24 00:15:00', tz = 'Etc/GMT+6'),
                                        datetime <= as.POSIXct('2022-03-28 00:00:00', tz = 'Etc/GMT+6'))
 
 CRHMr::writeObsFile(obs_event,
-                    '../../analysis/crhm-analysis/obs/ffr_crhm_modelling_obs_w_asm_data_fltr_event.obs',
+                    'crhm/obs/ffr_crhm_modelling_obs_w_asm_data_fltr_event.obs',
                     comment = 'This file consists of air temp, rh, and wind speed from the Waterloo Forest Tower (aka Fortress Forest Ride) see the R proj met-data-processing for qaqc and gap fill procedures. The radiation data is from Fortress Ridge and has been gap filled with Fortress Ridge South. Filtered to snowfall event from March 13, 2023 to March 20, 2023')
-
-# powerline input data found the timing of the wind at this station to be quite
-# off, ocassionally some better performance due to temp/RH but overall not a
-# huge difference. Decided to keep FFR forcing for validation. 
-pwl_obs <- readRDS('../../analysis/met-data-processing/data/pwl_crhm_modelling_obs.rds') |> 
-  # left_join(irtc) |> 
-  left_join(combined_tree_events) |> # change this if want cold events too
-  mutate(obs_snow_load = ifelse(is.na(obs_snow_load), 9999, obs_snow_load))
-CRHMr::writeObsFile(pwl_obs, '../../analysis/crhm-analysis/obs/pwl_crhm_modelling_obs_w_asm_data.obs')

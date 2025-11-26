48dd5faf7f41df6323637c99482b8064d415fafc
Alex Cebulski <alexcebulski@gmail.com>
2025-11-26 16:37:28 -0700

    changed var names

--- scripts/results/qunld-analysis/04-results-figs-combine-tbls.R
+++ scripts/results/qunld-analysis/04-results-figs-combine-tbls.R
@@ -7,7 +7,7 @@
 
 ### just wind and tau as in original manuscript ----
 wind_stats <- readRDS('data/results/modelled_wind_unloading_error_table.rds') |>
-  rename(Wind = Value) |>
+  rename(`Wind Speed` = Value) |>
   filter(Metric != 'Converged?')
 tau_stats <- readRDS('data/results/modelled_tau_unloading_error_table_gls.rds') |> rename(`Shear Stress` = Value)
 stats_out <- left_join(tau_stats, wind_stats) |> filter(Metric != 'Mean Absolute Error (mm/hr)')
@@ -22,11 +22,11 @@ load_stats <- readRDS('data/results/modelled_load_unloading_error_table_gls.rds'
 
 ## tau and some other vars
 tau_temp_stats <- readRDS('data/results/modelled_tau_temp_unloading_error_table.rds') |>
-  rename(`Shear Stress, Air Temp.` = Value) |> filter(Metric != 'Mean Absolute Error (mm/hr)')
+  rename(`Shear Stress, Air Temperature` = Value) |> filter(Metric != 'Mean Absolute Error (mm/hr)')
 tau_ibd_stats <- readRDS('data/results/modelled_tau_ibd_unloading_error_table.rds') |>
-  rename(`Shear Stress, Ice bulb dep.` = Value) |> filter(Metric != 'Mean Absolute Error (mm/hr)')
+  rename(`Shear Stress, Ice-Bulb dep.` = Value) |> filter(Metric != 'Mean Absolute Error (mm/hr)')
 tau_subl_stats <- readRDS('data/results/modelled_tau_subl_unloading_error_table.rds') |>
-  rename(`Shear Stress, Subl.` = Value) |> filter(Metric != 'Mean Absolute Error (mm/hr)')
+  rename(`Shear Stress, Sublimation` = Value) |> filter(Metric != 'Mean Absolute Error (mm/hr)')
 
 stats_out_all <- left_join(load_stats, stats_out) |>
   left_join(subl_stats) |> 
@@ -44,7 +44,7 @@ melt_stats <- readRDS('data/results/modelled_melt_unloading_melt_only_error_tabl
 air_temp_stats <- readRDS('data/results/modelled_melt_unloading_air_temp_error_table.rds') |>
   rename(`Air Temperature` = Value)
 ice_temp_stats <- readRDS('data/results/modelled_melt_unloading_ice_temp_error_table.rds') |>
-  rename(`Ice Bulb Temperature` = Value)
+  rename(`Ice-Bulb Temperature` = Value)
 melt_tau_stats <- readRDS('data/results/modelled_melt_unloading_melt_w_tau_error_table.rds') |>
   rename(`Dimensionless Snowmelt Rate, Shear Stress` = Value)
 
